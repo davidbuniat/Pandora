c0d609b67ba948f35928ad97bc496617b3a32dcc
JackHR <roper.jack.h@gmail.com>
2015-03-04 13:55:03 +0000

    Added csproj files to try aid project sharing

---
+++ Pandora/Assets/RotateToMouse.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateToMouse : MonoBehaviour {
+
+	public Vector3 targetPosition;
+	private Camera m_Cam;
+
+	void Awake () {
+
+		if(Camera.main != null)
+			m_Cam = Camera.main;
+		else
+			Debug.LogError("No main camera found");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		targetPosition = m_Cam.ScreenToWorldPoint(Input.mousePosition);
+
+	}
+}
