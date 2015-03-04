2dfad64bf0a491335c93976f20519230fd3365c8
JackHR <roper.jack.h@gmail.com>
2015-03-04 13:25:24 +0000

    Re-added Pandora project folder

---
+++ Pandora/Assets/Scripts/Movement.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Movement : MonoBehaviour {
+
+	public float speed = 5f; // default 5
+
+	private Rigidbody r_body;
+	[SerializeField]
+	private Vector3 verticalInput, horizontalInput;
+
+	void Awake()
+	{
+		r_body = gameObject.GetComponent<Rigidbody>();
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+		verticalInput = Input.GetAxis("Vertical");
+		horizontalInput = Input.GetAxis("Horizontal");
+	}
+
+	void FixedUpdate()
+	{
+		Vector3 velocity = r_body.velocity;
+		velocity.x = verticalInput * speed;
+		velocity.z = horizontalInput * speed;
+		r_body.velocity = velocity;
+	}
+}
