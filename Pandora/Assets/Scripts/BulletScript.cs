216fa557cc431a66e8cbed2d5c49d7033ad0b4dc
davidb <david.buniatyan@gmail.com>
2015-03-04 21:53:36 +0000

    Main Scene Ready

--- Pandora/Assets/Scripts/BulletScript.cs
+++ Pandora/Assets/Scripts/BulletScript.cs
@@ -7,6 +7,10 @@ public class BulletScript : MonoBehaviour {
 	void OnCollisionEnter(Collision other){
 
 		Debug.Log ("Hit " + other.gameObject.name);
+		Debug.Log(other.transform.tag);
+		if(other.transform.CompareTag("Enemy")){
+			Destroy(other.gameObject);
+		}
 		gameObject.SetActive (false);
 
 	}
