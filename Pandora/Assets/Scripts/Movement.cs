171383b9316086ef6f3d68f83100a82201154f25
JackHR <roper.jack.h@gmail.com>
2015-03-04 19:02:36 +0000

    animated robot

--- Pandora/Assets/Scripts/Movement.cs
+++ Pandora/Assets/Scripts/Movement.cs
@@ -7,12 +7,16 @@ public class Movement : MonoBehaviour {
 	public float speed = 10f; // default 10
 
 	private Rigidbody r_body;
+	private Animator animator;
+
+	public GameObject robobox;
 	[SerializeField]
 	private float verticalInput, horizontalInput;
 
 	void Awake()
 	{
 		r_body = gameObject.GetComponent<Rigidbody>();
+		animator = gameObject.GetComponent<Animator>();
 	}
 
 
@@ -20,13 +24,15 @@ public class Movement : MonoBehaviour {
 	void Update () {
 		verticalInput = Input.GetAxis("Vertical");
 		horizontalInput = Input.GetAxis("Horizontal");
+
+		animator.SetFloat("velocity", verticalInput);
 	}
 
 	void FixedUpdate()
 	{
 		Vector3 velocity = r_body.velocity;
 		velocity.z = verticalInput * speed;
-		velocity.x = horizontalInput * speed;
+		velocity.x = horizontalInput * speed;  
 		r_body.velocity = velocity;
 	}
 }
--- Pandora/Assets/Scripts/RotateToMouse.cs
+++ Pandora/Assets/Scripts/RotateToMouse.cs
@@ -3,23 +3,12 @@ using System.Collections;
 using UnityStandardAssets.CrossPlatformInput;
 
 public class RotateToMouse : MonoBehaviour {
-	
-
-	public GameObject cube;
-
-	[SerializeField]
-	private float xRotation, yRotation;
 
 	private Camera m_Cam;
-	[SerializeField]
-	private Vector3 targetPosition;
-
 	private Quaternion rotation;
 
+
 	void Awake () {
-
-		rotation = transform.localRotation;
-
 		if(Camera.main != null)
 			m_Cam = Camera.main;
 		else
@@ -36,16 +25,10 @@ public class RotateToMouse : MonoBehaviour {
 
 		Ray ray = m_Cam.ScreenPointToRay (Input.mousePosition);
 		RaycastHit hit;
-		if(Physics.Raycast(ray, out hit, Mathf.Infinity, LayerMask.NameToLayer("Player")))
+		if(Physics.Raycast(ray, out hit, Mathf.Infinity, LayerMask.NameToLayer("Environment")))
 		{
-			//transform.localRotation.SetFromToRotation(transform.position, hit.point);
-
-			//float targetRotation = Vector3.Angle(transform.position, hit.point);
-			//transform.localRotation = targetRotation;
 			transform.LookAt(hit.point);
-
-
-			//Vector
+			Debug.DrawLine(transform.position, hit.point, Color.red);
 		}
 	}
 }
