90c7d684ec06160ba2843ec03aa05426af67ef07
JackHR <roper.jack.h@gmail.com>
2015-03-04 12:01:08 +0000

    Robot dummy made

---
+++ Pandora/Assets/Scripts/Movement.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+
+	private Transform m_Cam;
+
+
+	void Awake()
+	{
+		if(Camera.main != null)
+		{
+			m_Cam = Camera.main.transform;
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
