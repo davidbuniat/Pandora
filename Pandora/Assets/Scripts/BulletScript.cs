80990e3793e1571cf7315bb7a0d3a67a223f282d
JackHR <roper.jack.h@gmail.com>
2015-03-06 15:38:47 +0000

    Removed some duplication

--- Pandora/Assets/Scripts/BulletScript.cs
+++ Pandora/Assets/Scripts/BulletScript.cs
@@ -6,13 +6,10 @@ public class BulletScript : MonoBehaviour {
 
 	void OnCollisionEnter(Collision other){
 
+		Debug.Log ("Hit " + other.gameObject.name);
 		if(other.gameObject.tag == "Enemy")
 			Destroy(other.gameObject);
-		Debug.Log ("Hit " + other.gameObject.name);
-		Debug.Log(other.transform.tag);
-		if(other.transform.CompareTag("Enemy")){
-			Destroy(other.gameObject);
-		}
+
 		gameObject.SetActive (false);
 
 	}
